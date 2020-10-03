3c15241984dff88df2ebf046447b983697d38840
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-03 12:10:39 -0300

    Initial Commit

---
+++ Nubank/NubankApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankApp.swift
+//  Nubank
+//
+//  Created by Raphael Cerqueira on 03/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NubankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
